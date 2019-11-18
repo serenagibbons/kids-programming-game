30eee1195972d7bbc5b94ac02cd7746e6a5c8622
Serena Gibbons <serenalgibbons@gmail.com>
2019-11-17 23:52:19 -0500

    Implement drag and drop on game arrows

--- app/src/main/java/com/example/kidsprogramminggame/GameActivity.java
+++ app/src/main/java/com/example/kidsprogramminggame/GameActivity.java
@@ -2,21 +2,16 @@ package com.example.kidsprogramminggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
-import android.graphics.Path;
-import android.graphics.drawable.AnimationDrawable;
-import android.media.Image;
 import android.os.Bundle;
-import android.transition.TransitionManager;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Objects;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -32,13 +27,15 @@ public class GameActivity extends AppCompatActivity {
             {R.drawable.arrow_right, R.drawable.arrow_down, R.drawable.arrow_right, R.drawable.arrow_down}
     };
 
+    private android.widget.LinearLayout.LayoutParams layoutParams;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
         // make activity full screen (hide actionbar)
-        if(getSupportActionBar()!=null)
+        if (getSupportActionBar() != null)
             this.getSupportActionBar().hide();
 
         robot = findViewById(R.id.imgRobot);
@@ -47,6 +44,111 @@ public class GameActivity extends AppCompatActivity {
         move2 = findViewById(R.id.imageDrop2);
         move3 = findViewById(R.id.imageDrop3);
         move4 = findViewById(R.id.imageDrop4);
+
+        right = findViewById((R.id.imageArrowRight));
+        left = findViewById((R.id.imageArrowLeft));
+        up = findViewById((R.id.imageArrowUp));
+        down = findViewById((R.id.imageArrowDown));
+
+        View.OnLongClickListener onLongClickListener = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+
+                ClipData dragData = new ClipData(v.getTag().toString(), mimeTypes, item);
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(v);
+
+                // start drag
+                v.startDragAndDrop(dragData, myShadow, null, 0);
+
+                // set view visibility to INVISIBLE when dragging
+                v.setVisibility(View.INVISIBLE);
+                return true;
+            }
+        };
+
+        View.OnDragListener onDragListener = new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        // Determines if this View can accept the dragged data
+                        return (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN));
+
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        // Invalidate the view to force a redraw in the new tint
+                        v.invalidate();
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        // Ignore the event
+
+                    case DragEvent.ACTION_DRAG_EXITED:
+
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        // Invalidates the view to force a redraw
+                        v.invalidate();
+                        return true;
+
+                    case DragEvent.ACTION_DROP:
+                        // Gets the item containing the dragged data
+                        ClipData.Item item = event.getClipData().getItemAt(0);
+
+                        // Invalidates the view to force a redraw
+                        v.invalidate();
+
+                        View view = (View) event.getLocalState();
+
+                        ((ImageView) v).setImageDrawable( ((ImageView)view).getDrawable());
+                        //((ImageView) v).setImageDrawable(((ImageView)event.getLocalState()).getBackground());
+                        // v.setBackground(((ImageView)event.getLocalState()).getBackground());
+
+                        view.setVisibility(View.VISIBLE);//finally set Visibility to VISIBLE
+                        return true;
+
+                    default:
+                        return true;
+                }
+            }
+        };
+
+        View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    ClipData data = ClipData.newPlainText("", "");
+                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+
+                    v.startDragAndDrop(data, shadowBuilder, v, 0);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+
+        right.setOnDragListener(onDragListener);
+        left.setOnDragListener(onDragListener);
+        up.setOnDragListener(onDragListener);
+        down.setOnDragListener(onDragListener);
+
+        right.setOnLongClickListener(onLongClickListener);
+        left.setOnLongClickListener(onLongClickListener);
+        up.setOnLongClickListener(onLongClickListener);
+        down.setOnLongClickListener(onLongClickListener);
+
+        right.setOnTouchListener(onTouchListener);
+        left.setOnTouchListener(onTouchListener);
+        up.setOnTouchListener(onTouchListener);
+        down.setOnTouchListener(onTouchListener);
+
+        move1.setOnDragListener(onDragListener);
+        move2.setOnDragListener(onDragListener);
+        move3.setOnDragListener(onDragListener);
+        move4.setOnDragListener(onDragListener);
     }
 
     // exit game, return to homepage
